9ec106cf27b2994586a43c63e78353d8b72636c6
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-06-30 18:50:46 +0300

    added markup for profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import user from "./data/user.json";
 import "./App.css";
 
+import SociallNetworkProfile from "./Components/SocialNetworkProfile/SocialNetworkProfile";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <SociallNetworkProfile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />
+  );
 }
 
 export default App;
